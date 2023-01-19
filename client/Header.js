fc87d419739ec46a80b0493237f1ec1dd30dafc5
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-01-19 04:43:13 -0500

    Centering divs and Dracula theme

--- client/Header.js
+++ client/Header.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='headContainer'>
-      <div className='header'>
-        <a href='#'>Snippets Collection</a>{' '}
-        <span className='welcome'>Welcome Back</span> <a href='#'>Logout</a>
-      </div>
+    <div className='header'>
+      <a href='#'>Snippets Collection</a>{' '}
+      <span className='welcome'>Welcome Back</span> <a href='#'>Logout</a>
     </div>
   );
 };
--- client/TextEditor.js
+++ client/TextEditor.js
@@ -1,18 +1,23 @@
 import React, { useRef, useState, useEffect } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { dracula } from '@uiw/codemirror-theme-dracula';
 
 const TextEditor = () => {
   const onChange = React.useCallback((value, viewUpdate) => {
     console.log('value:', value);
   }, []);
   return (
-    <CodeMirror
-      value="console.log('hello world!');"
-      height='200px'
-      extensions={[javascript({ jsx: true })]}
-      onChange={onChange}
-    />
+    <div className='textBox'>
+      <CodeMirror
+        value="console.log('hello world!');"
+        height='200px'
+        width='50vw'
+        extensions={[javascript({ jsx: true })]}
+        theme={dracula}
+        onChange={onChange}
+      />
+    </div>
   );
 };
 export default TextEditor;
