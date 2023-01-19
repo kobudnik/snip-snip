dd93c09bcbb6fb0f61bcde19ba964ae2ed1a8fbf
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-01-18 22:15:53 -0500

    HTML & CSS linked up. Time to build.

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 apiRouter.get('/', (req, res) => {
   console.log('in api router');
-  return res.status(200).json({ Hello: 'message' });
+  return res.status(200).json({ Hello: 'this is the api folder' });
 });
 
 module.exports = apiRouter;
--- server/server.js
+++ server/server.js
@@ -9,13 +9,8 @@ const PORT = 3000;
 const app = express();
 // app.use(express.static(path.resolve(__dirname, )))
 app.use(express.json());
-
 app.use('/api', apiRouter);
 
-app.get('/', (req, res) => {
-  res.status(200).json('HEllO');
-  // res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
-});
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
