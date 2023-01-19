497e27ce8628caa940217b5db025f9606dad68d2
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-01-19 02:34:47 -0500

    Database working w/ server

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const apiRouter = express.Router();
 const path = require('path');
+const snipController = require('../controllers/snipController');
 
 apiRouter.get('/', (req, res) => {
   console.log('in api router');
   return res.status(200).json({ Hello: 'this is the api folder' });
+});
+
+apiRouter.post('/', snipController.addUser, (req, res) => {
+  console.log('posting api router');
+  return res.status(200).json(res.locals.newUser);
 });
 
 module.exports = apiRouter;
