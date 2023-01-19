6204ebdc0ece13ec09918858454f34d2d70f1186
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-01-18 20:15:25 -0500

    React Getting Served

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const app = express();
 // app.use(express.static(path.resolve(__dirname, )))
 app.use(express.json());
 
-app.use(cors);
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
