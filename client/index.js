7974828073ea5216b34a0301cac55a1f5143b59f
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-03-28 06:20:51 -0400

    Formatted Nested Routes and Renamed Components

--- client/index.js
+++ client/index.js
@@ -1,5 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
+import Header from './Components/Header.jsx';
+import Home from './Components/Home.jsx';
+import Error from './Components/Error.jsx';
+import Signup from './Components/Signup.jsx';
+import Login from './Components/Login.jsx';
+// import CreateAccount from './routes/CreateAccount.jsx';
+// import Home, { eventLoader } from './routes/Home.jsx';
+// import Error from './components/Error.jsx';
+import './index.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Outlet,
+} from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return <div>{<Outlet></Outlet>}</div>;
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index element={<Login />}></Route>
+      <Route path='create' element={<Signup></Signup>}></Route>
+      <Route path='home' element={<Header />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Route>,
+  ),
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />,
+);
